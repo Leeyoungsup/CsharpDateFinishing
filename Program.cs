8f8ad19170d73da8890a4263981d024af86d4212
leeyoungsub <liive29252@gmail.com>
2020-06-05 14:34:08 +0900

    200605

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataFinshing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = (3 + 2) * 3 | 1 ^ 1;
+            //1. ++ --
+            //2. * / %
+            //3. + -
+            //4. << >>
+            //5. < >
+            //6. == !=
+            //7. &
+            //8. ^
+            //9. |
+            //..
+            var num = 3;
+            var num2 = "Hello World!";
+
+        }
+    }
+}
